8b9b0a2ce5f9c80d7f2da494c64e5d9e09c0865b
my-nozomi <mai.yokono@nozomi-ai.co.jp>
2025-04-25 18:44:02 +0900

    freeまとめ

---
+++ srcs/utils/cleanup.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 10:51:28 by myokono           #+#    #+#             */
+/*   Updated: 2025/04/23 10:53:38 by myokono          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+void	free_textures(t_game *game)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (game->tex[i].img.img)
+		{
+			mlx_destroy_image(game->mlx, game->tex[i].img.img);
+			game->tex[i].img.img = NULL;
+		}
+		if (game->tex[i].path)
+		{
+			free(game->tex[i].path);
+			game->tex[i].path = NULL;
+		}
+		i++;
+	}
+}
+
+void	free_map(t_map *map)
+{
+	int	i;
+
+	if (map->grid)
+	{
+		i = 0;
+		while (i < map->height)
+		{
+			if (map->grid[i])
+			{
+				free(map->grid[i]);
+				map->grid[i] = NULL;
+			}
+			i++;
+		}
+		free(map->grid);
+		map->grid = NULL;
+	}
+}
+
+void	cleanup_game(t_game *game)
+{
+	free_textures(game);
+	free_map(&game->map);
+	if (game->img.img)
+	{
+		mlx_destroy_image(game->mlx, game->img.img);
+		game->img.img = NULL;
+	}
+	if (game->win)
+	{
+		mlx_destroy_window(game->mlx, game->win);
+		game->win = NULL;
+	}
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+		game->mlx = NULL;
+	}
+}
+
+void	free_split(char **split)
+{
+	int	i;
+
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		split[i] = NULL;
+		i++;
+	}
+	free(split);
+}
