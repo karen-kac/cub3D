5e7deba8345e1a7a1f1e2c5b27d2439a01c5fbe7
karen-kac <mai.cn@akane.waseda.jp>
2025-04-29 15:42:20 +0900

    config_parserの整理

--- srcs/parser/config_parser.c
+++ srcs/parser/config_parser.c
@@ -12,125 +12,91 @@
 
 #include "cub3d.h"
 
-/* 設定情報の解析 */
 int parse_config(t_game *game, int fd)
 {
-    char *line;
-    int ret;
-    int count = 0;
+	char *line;
+	int ret;
+	int count = 0;
 
-    /* 必要な設定が揃うまで読み込み */
-    while (count < 6)  /* NO, SO, WE, EA, F, C の6つ */
-    {
-        ret = get_next_line(fd, &line);
-        if (ret <= 0)
-        {
-            if (line)
-                free(line);
-            return (error_msg(ERR_CONFIG));
-        }
+	while (count < 6)
+	{
+		ret = get_next_line(fd, &line);
+		if (ret <= 0)
+		{
+			if (line)
+				free(line);
+			return (error_msg(ERR_CONFIG));
+		}
+		if (line[0] == '\0')
+		{
+			free(line);
+			continue;
+		}
+		if (strncmp(line, "NO ", 3) == 0)
+			ret = parse_texture(game, line + 3, NORTH);
+		else if (strncmp(line, "SO ", 3) == 0)
+			ret = parse_texture(game, line + 3, SOUTH);
+		else if (strncmp(line, "WE ", 3) == 0)
+			ret = parse_texture(game, line + 3, WEST);
+		else if (strncmp(line, "EA ", 3) == 0)
+			ret = parse_texture(game, line + 3, EAST);
+		else if (strncmp(line, "F ", 2) == 0)
+			ret = parse_color(game, line + 2, 'F');
+		else if (strncmp(line, "C ", 2) == 0)
+			ret = parse_color(game, line + 2, 'C');
+		else
+			ret = 0;
+		if (ret)
+			count++;
+		free(line);
+		if (!ret)
+			return (0);
+	}
 
-        /* 空行はスキップ */
-        if (line[0] == '\0')
-        {
-            free(line);
-            continue;
-        }
-
-        /* 設定行の解析 */
-        if (strncmp(line, "NO ", 3) == 0)
-            ret = parse_texture(game, line + 3, NORTH);
-        else if (strncmp(line, "SO ", 3) == 0)
-            ret = parse_texture(game, line + 3, SOUTH);
-        else if (strncmp(line, "WE ", 3) == 0)
-            ret = parse_texture(game, line + 3, WEST);
-        else if (strncmp(line, "EA ", 3) == 0)
-            ret = parse_texture(game, line + 3, EAST);
-        else if (strncmp(line, "F ", 2) == 0)
-            ret = parse_color(game, line + 2, 'F');
-        else if (strncmp(line, "C ", 2) == 0)
-            ret = parse_color(game, line + 2, 'C');
-        else
-            ret = 0;
-
-        if (ret)
-            count++;
-
-        free(line);
-        if (!ret)
-            return (0);
-    }
-
-    return (1);
+	return (1);
 }
 
-/* テクスチャパスの解析 */
-int parse_texture(t_game *game, char *line, int dir)
+int	parse_texture(t_game *game, char *line, int dir)
 {
-    char *path;
-    
-    /* 先頭の空白をスキップ */
-    while (*line == ' ')
-        line++;
-    
-    /* パス文字列の複製 */
-    path = ft_strdup(line);
-    if (!path)
-        return (printf("here5"), error_msg(ERR_MEMORY));
-    
-    /* すでにパスが設定されていたら解放 */
-    if (game->tex[dir].path)
-    {
-        free(game->tex[dir].path);
-        game->tex[dir].path = NULL;
-    }
-    
-    /* パスを設定 */
-    game->tex[dir].path = path;
-    
-    return (1);
+	char	*path;
+	
+	while (*line == ' ')
+		line++;
+	path = ft_strdup(line);
+	if (!path)
+		return (printf("here5"), error_msg(ERR_MEMORY));
+	if (game->tex[dir].path)
+	{
+		free(game->tex[dir].path);
+		game->tex[dir].path = NULL;
+	}
+	game->tex[dir].path = path;
+	return (1);
 }
 
-/* 色の解析 */
 int parse_color(t_game *game, char *line, char type)
 {
-    int r, g, b;
-    char **rgb;
-    
-    /* 先頭の空白をスキップ */
-    while (*line == ' ')
-        line++;
-    
-    /* RGBを分割 */
-    rgb = ft_split(line, ',');
-    if (!rgb)
-        return (printf("here4"), error_msg(ERR_MEMORY));
-    
-    /* RGB値の取得 */
-    if (!rgb[0] || !rgb[1] || !rgb[2] || rgb[3])
-    {
-        free_split(rgb);
-        return (error_msg(ERR_CONFIG));
-    }
-    
-    /* RGB値の解析 */
-    r = ft_atoi(rgb[0]);
-    g = ft_atoi(rgb[1]);
-    b = ft_atoi(rgb[2]);
-    
-    /* RGB値の範囲チェック */
-    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
-    {
-        free_split(rgb);
-        return (error_msg(ERR_CONFIG));
-    }
-    
-    /* 色を設定 */
-    if (type == 'F')
-        game->floor_color = rgb_to_int(r, g, b);
-    else if (type == 'C')
-        game->ceiling_color = rgb_to_int(r, g, b);
-    
-    free_split(rgb);
-    return (1);
+	int		r;
+	int		g;
+	int		b;
+	char	**rgb;
+
+	while (*line == ' ')
+		line++;
+	rgb = ft_split(line, ',');
+	if (!rgb)
+		return (printf("here4"), error_msg(ERR_MEMORY));
+	if (!rgb[0] || !rgb[1] || !rgb[2] || rgb[3])
+		return (free_split(rgb), error_msg(ERR_CONFIG));
+	r = ft_atoi(rgb[0]);
+	g = ft_atoi(rgb[1]);
+	b = ft_atoi(rgb[2]);
+	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+		return (free_split(rgb), error_msg(ERR_CONFIG));
+	if (type == 'F')
+		game->floor_color = rgb_to_int(r, g, b);
+	else if (type == 'C')
+		game->ceiling_color = rgb_to_int(r, g, b);
+	free_split(rgb);
+	return (1);
 }
