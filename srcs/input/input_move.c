53682091328189724fc114b14b3106c144b7fdfb
karen-kac <mai.cn@akane.waseda.jp>
2025-04-29 18:29:28 +0900

    norm

--- srcs/input/input_move.c
+++ srcs/input/input_move.c
@@ -6,7 +6,7 @@
 /*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 18:20:02 by myokono           #+#    #+#             */
-/*   Updated: 2025/04/29 18:22:27 by myokono          ###   ########.fr       */
+/*   Updated: 2025/04/29 18:28:54 by myokono          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,40 @@
 
 void	move_forward(t_game *game)
 {
-	if (game->map.grid[(int)game->player.pos_y][(int)(game->player.pos_x + game->player.dir_x * MOVE_SPEED)] != '1')
+	if (game->map.grid[(int)game->player.pos_y][(int)(game->player.pos_x
+		+ game->player.dir_x * MOVE_SPEED)] != '1')
 		game->player.pos_x += game->player.dir_x * MOVE_SPEED;
-	if (game->map.grid[(int)(game->player.pos_y + game->player.dir_y * MOVE_SPEED)][(int)game->player.pos_x] != '1')
+	if (game->map.grid[(int)(game->player.pos_y
+			+ game->player.dir_y * MOVE_SPEED)][(int)game->player.pos_x] != '1')
 		game->player.pos_y += game->player.dir_y * MOVE_SPEED;
 }
 
 void	move_backward(t_game *game)
 {
-	if (game->map.grid[(int)game->player.pos_y][(int)(game->player.pos_x - game->player.dir_x * MOVE_SPEED)] != '1')
+	if (game->map.grid[(int)game->player.pos_y]
+		[(int)(game->player.pos_x - game->player.dir_x * MOVE_SPEED)] != '1')
 		game->player.pos_x -= game->player.dir_x * MOVE_SPEED;
-	if (game->map.grid[(int)(game->player.pos_y - game->player.dir_y * MOVE_SPEED)][(int)game->player.pos_x] != '1')
+	if (game->map.grid[(int)(game->player.pos_y
+			- game->player.dir_y * MOVE_SPEED)][(int)game->player.pos_x] != '1')
 		game->player.pos_y -= game->player.dir_y * MOVE_SPEED;
 }
 
 void	strafe_left(t_game *game)
 {
-	if (game->map.grid[(int)game->player.pos_y][(int)(game->player.pos_x + game->player.dir_y * MOVE_SPEED)] != '1')
+	if (game->map.grid[(int)game->player.pos_y]
+		[(int)(game->player.pos_x + game->player.dir_y * MOVE_SPEED)] != '1')
 		game->player.pos_x += game->player.dir_y * MOVE_SPEED;
-	if (game->map.grid[(int)(game->player.pos_y - game->player.dir_x * MOVE_SPEED)][(int)game->player.pos_x] != '1')
+	if (game->map.grid[(int)(game->player.pos_y
+			- game->player.dir_x * MOVE_SPEED)][(int)game->player.pos_x] != '1')
 		game->player.pos_y -= game->player.dir_x * MOVE_SPEED;
 }
 
 void	strafe_right(t_game *game)
 {
-	if (game->map.grid[(int)game->player.pos_y][(int)(game->player.pos_x - game->player.dir_y * MOVE_SPEED)] != '1')
+	if (game->map.grid[(int)game->player.pos_y]
+		[(int)(game->player.pos_x - game->player.dir_y * MOVE_SPEED)] != '1')
 		game->player.pos_x -= game->player.dir_y * MOVE_SPEED;
-	if (game->map.grid[(int)(game->player.pos_y + game->player.dir_x * MOVE_SPEED)][(int)game->player.pos_x] != '1')
+	if (game->map.grid[(int)(game->player.pos_y
+			+ game->player.dir_x * MOVE_SPEED)][(int)game->player.pos_x] != '1')
 		game->player.pos_y += game->player.dir_x * MOVE_SPEED;
 }
--- srcs/input/input_rotate.c
+++ srcs/input/input_rotate.c
@@ -6,7 +6,7 @@
 /*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 18:20:46 by myokono           #+#    #+#             */
-/*   Updated: 2025/04/29 18:22:13 by myokono          ###   ########.fr       */
+/*   Updated: 2025/04/29 18:26:13 by myokono          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,15 @@ void	rotate_left(t_game *game)
 	double	old_plane_x;
 
 	old_dir_x = game->player.dir_x;
-	game->player.dir_x = game->player.dir_x * cos(-ROT_SPEED) - game->player.dir_y * sin(-ROT_SPEED);
-	game->player.dir_y = old_dir_x * sin(-ROT_SPEED) + game->player.dir_y * cos(-ROT_SPEED);
+	game->player.dir_x = game->player.dir_x * cos(-ROT_SPEED)
+		- game->player.dir_y * sin(-ROT_SPEED);
+	game->player.dir_y = old_dir_x * sin(-ROT_SPEED)
+		+ game->player.dir_y * cos(-ROT_SPEED);
 	old_plane_x = game->player.plane_x;
-	game->player.plane_x = game->player.plane_x * cos(-ROT_SPEED) - game->player.plane_y * sin(-ROT_SPEED);
-	game->player.plane_y = old_plane_x * sin(-ROT_SPEED) + game->player.plane_y * cos(-ROT_SPEED);
+	game->player.plane_x = game->player.plane_x * cos(-ROT_SPEED)
+		- game->player.plane_y * sin(-ROT_SPEED);
+	game->player.plane_y = old_plane_x * sin(-ROT_SPEED)
+		+ game->player.plane_y * cos(-ROT_SPEED);
 }
 
 void	rotate_right(t_game *game)
@@ -31,9 +35,13 @@ void	rotate_right(t_game *game)
 	double	old_plane_x;
 
 	old_dir_x = game->player.dir_x;
-	game->player.dir_x = game->player.dir_x * cos(ROT_SPEED) - game->player.dir_y * sin(ROT_SPEED);
-	game->player.dir_y = old_dir_x * sin(ROT_SPEED) + game->player.dir_y * cos(ROT_SPEED);
+	game->player.dir_x = game->player.dir_x * cos(ROT_SPEED)
+		- game->player.dir_y * sin(ROT_SPEED);
+	game->player.dir_y = old_dir_x * sin(ROT_SPEED)
+		+ game->player.dir_y * cos(ROT_SPEED);
 	old_plane_x = game->player.plane_x;
-	game->player.plane_x = game->player.plane_x * cos(ROT_SPEED) - game->player.plane_y * sin(ROT_SPEED);
-	game->player.plane_y = old_plane_x * sin(ROT_SPEED) + game->player.plane_y * cos(ROT_SPEED);
+	game->player.plane_x = game->player.plane_x * cos(ROT_SPEED)
+		- game->player.plane_y * sin(ROT_SPEED);
+	game->player.plane_y = old_plane_x * sin(ROT_SPEED)
+		+ game->player.plane_y * cos(ROT_SPEED);
 }
