547c6a7440ee4097d8f315d11fd6716a878a94b2
karen-kac <mai.cn@akane.waseda.jp>
2025-04-29 15:44:15 +0900

    mainからutilsにis_valid_extension関数を移動しました

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -12,6 +12,22 @@
 
 #include "cub3d.h"
 
+bool	is_valid_extension(char *filename, char *ext)
+{
+	int	filename_len;
+	int	ext_len;
+
+	if (!filename || !ext)
+		return (false);
+	filename_len = ft_strlen(filename);
+	ext_len = ft_strlen(ext);
+	if (filename_len <= ext_len)
+		return (false);
+	if (ft_strncmp(filename + filename_len - ext_len, ext, ext_len) != 0)
+		return (false);
+	return (true);
+}
+
 int	error_msg(char *msg)
 {
 	ft_putendl_fd(msg, 2);
