de3e4e765fb5589a52d6594f395c57e889b28892
karen-kac <mai.cn@akane.waseda.jp>
2025-04-29 16:14:49 +0900

    find_player関数の分割

--- srcs/parser/player.c
+++ srcs/parser/player.c
@@ -6,19 +6,30 @@
 /*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 15:58:40 by myokono           #+#    #+#             */
-/*   Updated: 2025/04/29 15:59:18 by myokono          ###   ########.fr       */
+/*   Updated: 2025/04/29 16:14:08 by myokono          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static int	set_player_info(t_game *game, int x, int y, int *player_found)
+{
+	if (*player_found)
+		return (error_msg(ERR_MAP));
+	game->map.player_x = x;
+	game->map.player_y = y;
+	game->map.player_dir = game->map.grid[y][x];
+	game->map.grid[y][x] = '0';
+	*player_found = 1;
+	return (1);
+}
 
 int	find_player(t_game *game)
 {
 	int	i;
 	int	j;
 	int	player_found;
-	
+
 	player_found = 0;
 	i = 0;
 	while (i < game->map.height)
@@ -28,13 +39,8 @@ int	find_player(t_game *game)
 		{
 			if (is_player(game->map.grid[i][j]))
 			{
-				if (player_found)
-					return (error_msg(ERR_MAP));
-				game->map.player_x = j;
-				game->map.player_y = i;
-				game->map.player_dir = game->map.grid[i][j];
-				game->map.grid[i][j] = '0';
-				player_found = 1;
+				if (!set_player_info(game, j, i, &player_found))
+					return (0);
 			}
 			j++;
 		}
